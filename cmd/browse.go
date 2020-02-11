b31f282297a2feba473036c2aae249938bc1fd0a
Takumasa Sakao <sakataku7@gmail.com>
2020-02-12 01:43:15 +0900

    Add browse cmd

---
+++ cmd/browse.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/pkg/browser"
+	"github.com/sachaos/atcoder/files"
+	"github.com/spf13/cobra"
+)
+
+// browseCmd represents the browse command
+var browseCmd = &cobra.Command{
+	Use:   "browse DIRECTORY",
+	Short: "browse AtCoder task page",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("please specify directory path")
+		}
+		dir := args[0]
+
+		conf, err := files.LoadConf(dir)
+		if err != nil {
+			return err
+		}
+
+		return browser.OpenURL(conf.AtCoder.TaskURL)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(browseCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// browseCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// browseCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gookit/color v1.2.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pelletier/go-toml v1.6.0
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
 	github.com/rakyll/statik v0.1.6
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
